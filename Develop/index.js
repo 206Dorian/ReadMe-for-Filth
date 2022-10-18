df6f46b296b9d8543445878e719861b3562f079f
Dorian Birch <206dorian@gmail.com>
2022-10-18 16:30:11 -0500

    deleted readme test

--- Develop/index.js
+++ Develop/index.js
@@ -114,16 +114,4 @@ Email me with any questions [${email}](mailto:${email}) <br/>
 [Here is a link to my Github](https://github.com/${github}) for other projects
 
 `
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
-
-
-// Function call to initialize app
-// Creating a Clone, but where does it go?
 
